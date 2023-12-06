8ff8fe45b8935ce95351c64f8fef96fa9965e70c
Titus Taylor <ttaylor29@my.apsu.edu>
2023-12-06 11:21:35 -0600

    Add project files.

---
+++ DemoToStackOverflow.Client/Program.cs
@@ -0,0 +1,14 @@
+﻿using DemoToStackOverflow.Client;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Syncfusion.Blazor;
+
+//Register Syncfusion license https://help.syncfusion.com/common/essential-studio/licensing/how-to-generate
+//Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("YOUR LICENSE KEY");
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+builder.Services.AddSyncfusionBlazor();
+builder.Services.AddAuthorizationCore();
+builder.Services.AddCascadingAuthenticationState();
+builder.Services.AddSingleton<AuthenticationStateProvider, PersistentAuthenticationStateProvider>();
+await builder.Build().RunAsync();
